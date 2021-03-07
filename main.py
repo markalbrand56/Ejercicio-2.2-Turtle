bf095888a115fcac70225eccb1a3e680d117df35
Mark Albrand <mark.albrand5@gmail.com>
2021-03-06 19:22:33 -0600

    Definición de las funciones a utilizar. Estructura de la parte de análisis

--- main.py
+++ main.py
@@ -3,3 +3,23 @@
 # CoAutor: Alejandro Azurdia
 # Fecha: 6-03-2021
 # Programa para dibujar una jirafa a base de figuras geométricas
+
+def triangulo_equilatero():
+    pass
+
+
+def cuadrado():
+    pass
+
+
+def rombo_regular():  # dos triángulos equiláteros
+    pass
+
+
+def rombo_extendido(): # dos triangulos isóceles
+    pass
+
+
+def hexagono():
+    pass
+
